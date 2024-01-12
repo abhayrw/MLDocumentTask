e7df65d7622364c70c69c9dafc063b7b0710e5ae
abhayrw <156295076+abhayrw@users.noreply.github.com>
2024-01-12 16:02:54 +0530

    Update app_fastapi.py

--- app_fastapi.py
+++ app_fastapi.py
@@ -27,8 +27,12 @@ class DocumentClassifier(nn.Module):
 
 # Load the pre-trained model
 model = DocumentClassifier(num_classes=4)
-model.load_state_dict(torch.load("/app/model/model.pth"))  # Specify the correct path
-model.eval()
+try:
+    # Load the state dictionary
+    model.load_state_dict(torch.load("/app/model/model.pth"))
+    print("Model loaded successfully!")
+except Exception as e:
+    print("Error loading the model:", e)
 
 document_types = {0: "Pan Card", 1: "Aadhar Card", 2: "Passport", 3: "Voter ID"}
 
